3043066661eac64aabf0fa28f90165aadcbca082
Italo Alves <>
2023-12-21 14:24:31 -0300

    Finaliza modulo 4

---
+++ Modulo 4/webpack.config.dev.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [new TerserPlugin()],
+};
